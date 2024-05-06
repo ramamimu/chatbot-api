17258f2a704ca96a2ed57d9e95e9c5207d176954
ramamimu <ahm.f.ramadhani@gmail.com>
2024-05-06 13:04:33 +0700

    chore: version checker

---
+++ version_checker.py
@@ -0,0 +1,15 @@
+import fastapi
+# 0.111.0
+print(fastapi.__version__)
+
+import lorem_text
+# 2.1
+print(lorem_text.__version__)
+
+import requests
+# 2.31.0
+print(requests.__version__)
+
+import sse_starlette
+# 2.1.0
+print(sse_starlette.__version__)
