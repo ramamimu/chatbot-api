b10cd956568ced51f38484e41e801a69ddea3707
ramamimu <ahm.f.ramadhani@gmail.com>
2024-06-03 21:10:56 +0700

    refactor: Files entity datetimo to ISO with 'z' in the end

--- src/commons/types/files_db_type.py
+++ src/commons/types/files_db_type.py
@@ -1,9 +1,25 @@
-from pydantic import BaseModel
-from datetime import datetime
+from pydantic import BaseModel, field_validator, model_validator
+from datetime import datetime, timezone
 
 class FilesEntity(BaseModel):
     id: int
     name: str
     filename: str
-    created: datetime
-    lastModified: datetime
+    created: str
+    lastModified: str
+
+    @model_validator(mode="before")
+    def convert_datetimes_to_strings(cls, values):
+        for dt_field in ['created', 'lastModified']:
+            if isinstance(values.get(dt_field), datetime):
+                dt = values.get(dt_field)
+                if dt.tzinfo is None:
+                    dt = dt.replace(tzinfo=timezone.utc)
+                values[dt_field] = dt.astimezone(timezone.utc).isoformat().replace('+00:00', 'Z')
+        return values
+
+    class Config:
+        orm_mode = True
+
+    class Config:
+        orm_mode = True
