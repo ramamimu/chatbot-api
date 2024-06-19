760576a2c1c536276990702b52cd5b39bdb52491
ramamimu <ahm.f.ramadhani@gmail.com>
2024-06-19 14:13:58 +0700

    feat: error handler

--- src/api/files/__init__.py
+++ src/api/files/__init__.py
@@ -1,6 +1,8 @@
 from src.api.files.handler import FilesHandler
+from src.api.files.file_utils import FileUtils
 from src.api.files.routes import routes
 
 def register(file_storage_service, files_db_service, embedding_service, vectorstore_service):
-  files_handler = FilesHandler(file_storage_service, files_db_service, embedding_service, vectorstore_service)
+  file_utils = FileUtils()
+  files_handler = FilesHandler(file_utils, file_storage_service, files_db_service, embedding_service, vectorstore_service)
   return routes(files_handler)
